4a9a0bf52b04be59611f2284d2444e10d6828a96
ayumitanaka <tanakayumi13@gmail.com>
2021-05-09 18:49:27 +0000

    added links

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,27 @@ const Modal = ({ display, onClick }) => {
     <div className={`${display} Modal`} onClick={onClick}>
       <div className="text-center">
         <h1>Thank you!</h1>
-        <p className="mt-16">You nominated 5 lovely movies!</p>
+        <p className="mt-16 mb-6">You nominated 5 lovely movies!</p>
+        <small>
+          2021 ©︎ Your Cinema Paradiso by{" "}
+          <a
+            href="https://ayumitanaka.net/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Ayumi Tanaka
+          </a>
+          <br />
+          <a
+            href="https://github.com/ayumitanaka13/cinema-paradiso"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Check source code on Github
+          </a>
+        </small>
       </div>
     </div>
   );
